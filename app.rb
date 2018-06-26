c1f484097b47c78b5627a8203ccdb8de5e0c42c5
Ashley Minor <aminor@kipphouston.org>
2018-06-26 21:50:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+get '/' do
+  erb :index
+end
+
+get '/hours' do
+  erb :hours
+end
+
+get '/tournaments' do
+  erb :tournaments
+end
+
+get '/menu' do
+  erb :hours
+end
 
 end
